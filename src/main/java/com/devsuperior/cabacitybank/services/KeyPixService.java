f04f4c851e52534e2b76d529d2816d1078021e6d
Adriel Ryan <adrielryan.tj@hotmail.com>
2023-10-25 15:43:52 -0300

    Finalizando KeyPixService

--- src/main/java/com/devsuperior/cabacitybank/services/KeyPixService.java
+++ src/main/java/com/devsuperior/cabacitybank/services/KeyPixService.java
@@ -1,15 +1,32 @@
 package com.devsuperior.cabacitybank.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+
+import com.devsuperior.cabacitybank.dto.KeyPixDTO;
+import com.devsuperior.cabacitybank.dto.KeyPixMinDTO;
 import com.devsuperior.cabacitybank.entities.KeyPix;
+import com.devsuperior.cabacitybank.repositories.KeyPixRepository;
 
 @Service
 public class KeyPixService {
 
   @Autowired
-  private KeyPixService keyPixService;
+  private KeyPixRepository keyPixRepository;
 
-  public KeyPix findById(Long)
+  @Transactional(readOnly = true)
+  public KeyPixDTO findById(Long id) {
+    KeyPix result = keyPixRepository.findById(id).get();
+    return new KeyPixDTO(result);
+  }
+
+  @Transactional(readOnly = true)
+  public List<KeyPixMinDTO> findAll() {
+    List<KeyPix> result = keyPixRepository.findAll();
+    return result.stream().map(x -> new KeyPixMinDTO(x)).toList();
+  }
 }
