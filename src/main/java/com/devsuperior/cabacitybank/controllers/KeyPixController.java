0aa8e94a5565465e775e04e533d834a70b5352ef
Adriel Ryan <adrielryan.tj@hotmail.com>
2023-10-25 15:54:48 -0300

    Finalizando estruturação do KeyPixController

--- src/main/java/com/devsuperior/cabacitybank/controllers/KeyPixController.java
+++ src/main/java/com/devsuperior/cabacitybank/controllers/KeyPixController.java
@@ -1,18 +1,32 @@
 package com.devsuperior.cabacitybank.controllers;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.cabacitybank.dto.KeyPixDTO;
+import com.devsuperior.cabacitybank.dto.KeyPixMinDTO;
+import com.devsuperior.cabacitybank.services.KeyPixService;
 
 @RestController
 @RequestMapping(value = "/keypix")
 public class KeyPixController {
 
   @Autowired
-  private KeyPixController keyPixController;
+  private KeyPixService keyPixService;
 
+  @GetMapping(value = "/{id}")
   public KeyPixDTO findById(@PathVariable Long id) {
     KeyPixDTO result = keyPixService.findById(id);
+    return result;
+  }
+
+  @GetMapping
+  public List<KeyPixMinDTO> findAll() {
+    List<KeyPixMinDTO> result = keyPixService.findAll();
     return result;
   }
 
